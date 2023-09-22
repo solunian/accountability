40cc5d91c20153de37451bc0ba578f35b494e71b
Daniel Cui <solunian@gmail.com>
2023-09-22 15:20:15 -0700

    init swiftui project

---
+++ app/accountability/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  accountability
+//
+//  Created by Daniel Cui on 2023-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/accountability/accountabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountabilityApp.swift
+//  accountability
+//
+//  Created by Daniel Cui on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct accountabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
